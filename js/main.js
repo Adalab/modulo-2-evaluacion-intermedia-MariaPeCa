59e071b9ffd29fe8e026ad7fa00da12a3290b936
MariaPeCa <mpenacantero@gmail.com>
2023-05-31 10:10:55 +0200

    add function randomNumber

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@
 const select = document.querySelector('.js_select');
 const input = document.querySelector('.js_input');
 const btn = document.querySelector('.js_btn');
-
+const message = document.querySelector('.js_msg');
 //condicionales, funciones
+
+//si el select coincide con el numero random entonces al hacer click en el bton "jugar" se muestra mensaje "has ganado..." en message.
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random()* max);
@@ -15,3 +17,12 @@ function getRandomNumber(max) {
 
 //listener y eventos
 //btn Jugar evento click
+function play() {
+    const randomNumber = getRandomNumber(6); 
+if (randomNumber === select) {
+    message.innerHTML = "Has ganado el doble de lo apostado"
+}else{
+    message.innerHTML = "Has perdido lo apostado"
+}
+
+}
